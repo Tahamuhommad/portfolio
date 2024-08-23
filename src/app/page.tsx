e2ef75840fff0357955ed85158f1645a3cad373d
Tahamuhommad <muhommadtaha52@gmail.com>
2024-08-23 14:39:41 +0500

    next.js

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <div className="bg-gradient-to-tr from-blue-950 to-teal-950">      
+    <div className="bg-gradient-to-tr from-blue-950 to-teal-950 text-white">      
       <h1 className="font-serif size-9">WELCOME</h1>
       <p>This is my portfolio website.This is made by me by using next.js.To see more about me click go to about 
         
